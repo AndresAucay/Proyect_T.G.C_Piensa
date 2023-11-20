f1c9214c1df9703d39b409c32c69ad9cd344d537
Andres <maaucay@sudamericano.edu.ec>
2023-11-20 16:05:30 -0500

    first commit

---
+++ src/test/kotlin/com/anonimous/T/G/C/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anonimous.T.G.C
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
